30d6df85a46cb1eca0cb78dbd3d62ff5f753d1dd
Spongey21 <driton.latifi101@gmail.com>
2023-03-08 12:19:32 +0100

    comments

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 function forumlar(form) {
-  for (let i = 0; i < form.length - 1; i++) {
-    // checks all elements within form except the submit button
-    if (form[i].value.length == 0) {
-      // if input is empty
+  for (let i = 0; i < form.length - 1; i++) { // checks all elements within form except the submit button
+    if (form[i].value.length == 0) { // if input is empty
       console.log(`Udfyld ${form[i].name}!`);
 
       form[i].focus();
 
       return false;
-    } else if (!isNaN(form[i].value) && form[i].value.length === 4 && form[i].id == "post") {
-      form[i].style.backgroundColor = "lightGreen"; // 4-digit number with id of post
-    } else if (emailCheck(form[i])) {
+    } else if (!isNaN(form[i].value) && form[i].value.length === 4 && form[i].id == "post") { // 4-digit number with id of post
+      form[i].style.backgroundColor = "lightGreen"; 
+    } else if (emailCheck(form[i])) { // emailCheck function
         form[i].style.backgroundColor = "Green";
-    } else {
-      form[i].style.backgroundColor = "lightBlue"; // alt andet
+    } else { // everything else
+      form[i].style.backgroundColor = "lightBlue";
     }
   }
   return true;
 }
 
-function emailCheck(email) {
+function emailCheck(email) { // checks if the id is email, then via regex finds the speficied characters that occur in an email
     if (email.id == 'email' && /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email.value)) {
         console.log(email.id + ': found');
         return true;
